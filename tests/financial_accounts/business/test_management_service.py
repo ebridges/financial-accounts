a3c7bc8c4c5a32d6647175ddde12855da29db886
Edward Bridges <github@eqbridges.com>
2025-01-26 16:20:44 -0500

    cleanup: update management service tests

--- tests/financial_accounts/business/test_management_service.py
+++ tests/financial_accounts/business/test_management_service.py
@@ -20,52 +20,53 @@ def db_url():
     # We don't need to keep the file descriptor open
     os.close(fd)
 
-    url = f"sqlite:///{path}"
+    url = f"sqlite:///{path}?mode=rw&uri=true"
 
     try:
         yield url
     finally:
         # Remove the file after the test is done
         os.remove(path)
+    # return 'sqlite:///:memory:'
 
 
 def test_export_account_hierarchy_as_json(db_url):
-    mgmt_service = ManagementService().init_with_url(db_url)
-    mgmt_service.reset_database()
-
-    # Create a sample account hierarchy
-    with BookService().init_with_url(db_url) as book_service:
+    with (
+        ManagementService().init_with_url(db_url) as management_service,
+        BookService().init_with_url(db_url) as book_service,
+        AccountService().init_with_url(db_url) as account_service,
+    ):
+        management_service.reset_database()
         book = book_service.create_new_book("Test Book")
-        with AccountService().init_with_session(book_service.SessionLocal) as account_service:
-            root_account = account_service.add_account(
-                book_name=book.name,
-                parent_code=None,
-                parent_name=None,
-                acct_name="Root Account",
-                full_name="Root Account",
-                acct_code="000",
-                acct_type="ROOT",
-                description="Root account",
-                hidden=False,
-                placeholder=False,
-            )
-            print(f'Created root account: {root_account.id}')
-            child_account = account_service.add_account(
-                book_name=book.name,
-                parent_code=root_account.code,
-                parent_name=root_account.name,
-                acct_name="Child Account",
-                full_name="Root Account:Child Account",
-                acct_code="001",
-                acct_type="ASSET",
-                description="Child account",
-                hidden=False,
-                placeholder=False,
-            )
-            print(f'Created child account: {child_account.id}')
+        root_account = account_service.add_account(
+            book_name=book.name,
+            parent_code=None,
+            parent_name=None,
+            acct_name="Root Account",
+            full_name="Root Account",
+            acct_code="000",
+            acct_type="ROOT",
+            description="Root account",
+            hidden=False,
+            placeholder=False,
+        )
+        print(f'Created root account: {root_account.id}')
+        child_account = account_service.add_account(
+            book_name=book.name,
+            parent_code=root_account.code,
+            parent_name=root_account.name,
+            acct_name="Child Account",
+            full_name="Root Account:Child Account",
+            acct_code="001",
+            acct_type="ASSET",
+            description="Child account",
+            hidden=False,
+            placeholder=False,
+        )
+        print(f'Created child account: {child_account.id}')
 
-    # Export the account hierarchy as JSON
-    json_output = mgmt_service.export_account_hierarchy_as_json()
+        # Export the account hierarchy as JSON
+        json_output = management_service.export_account_hierarchy_as_json()
 
     # Verify the JSON output
     hierarchy = json.loads(json_output)
@@ -77,9 +78,12 @@ def test_export_account_hierarchy_as_json(db_url):
 
 def test_reset_database(db_url):
     test_book_name = 'test_book_name'
-    with BookService().init_with_url(db_url) as service:
-        Base.metadata.create_all(service.engine)
-        service.create_new_book(test_book_name)
-        mgmt_service = ManagementService().init_with_engine(service.engine)
-        mgmt_service.reset_database()
-        assert not service.get_book_by_name(test_book_name)
+    with (
+        ManagementService().init_with_url(db_url) as management_service,
+        BookService().init_with_url(db_url) as book_service,
+    ):
+
+        Base.metadata.create_all(book_service.engine)
+        book_service.create_new_book(test_book_name)
+        management_service.reset_database()
+        assert not book_service.get_book_by_name(test_book_name)
